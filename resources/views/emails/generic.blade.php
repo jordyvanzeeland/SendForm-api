7265a38a2c9b42e8e517335458a1b43d65c2b7c7
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2026-01-13 17:08:14 +0100

    Edited mail template

--- resources/views/emails/generic.blade.php
+++ resources/views/emails/generic.blade.php
@@ -1,3 +1,8 @@
+<p>Iemand heeft het contactformulier op je website ingevuld:</p>
+
 <p>
+    <strong>Van:</strong> {{ $from_name }} ({{ $from_email }}) <br /><br />
+    <strong>Bericht</strong>: <br />
+    
     {{ $content }}
 </p>
